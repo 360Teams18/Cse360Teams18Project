3f5d2ad2968af580dd9c2608a9e76ab96e7e0252
mlheaton-asu <mlheaton@asu.edu>
2016-04-13 11:49:59 -0700

    Added files via upload

---
+++ AirplanePanelTest.java
@@ -0,0 +1,15 @@
+package Game;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AirplanePanelTest {
+
+	@Test
+	public void testAirplanePanel() {
+		AirplanePanel tester = new AirplanePanel();
+		assertNotNull(tester);
+	}
+
+}
