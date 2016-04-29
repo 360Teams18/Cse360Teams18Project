8ca35f3d382aafb89c18d397e1dc9c75114894dd
mlheaton-asu <mlheaton@asu.edu>
2016-04-29 11:58:34 -0700

    Update AirplaneGame.java

--- Team18_Final/src/Game/AirplaneGame.java
+++ Team18_Final/src/Game/AirplaneGame.java
@@ -1,4 +1,4 @@
-/** IMPORTANT NOTE: In order to run properly the text file 'stats.txt' must be included
+/** IMPORTANT NOTE: In order to run properly the text file 'stat.txt' must be included
  * in the proper location.
 */
 
